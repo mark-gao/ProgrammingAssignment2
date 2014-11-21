816cdf535578e9bb7e476579ee505c77650d16a9
Yilong Gao(Mark) <mark.gao@gmail.com>
2014-11-21 15:05:43 -0700

    Completed code of assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,49 @@
 ## Put comments here that give an overall description of what your
-## functions do
 
-## Write a short comment describing this function
+## R Programming - Assignment 2
+## Cache a matrix's inversion in a different environment when the first time to calculate it
+## Use cached inversion the next time
+
+###  Yilong Gao (Mark)
+###  November 21, 2014
+
+## function makeCacheMatrix(): cache matrix x and its inversion
+## No inversion calculation in this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    mi <- NULL  ## matrix inversion
+    
+    set <- function(y) {
+        x <<- y
+        mi <<- NULL
+    }
+    
+    get <- function() x
+    
+    setInversion <- function(Inversion) mi <<- Inversion
+    
+    getInversion <- function() mi
+    
+    list(set = set, get = get,
+         setInversion = setInversion,
+         getInversion = getInversion)
 }
 
 
-## Write a short comment describing this function
+## Function cacheSolve(): calculate a matrix's inversion if it is not in cache; otherwise use cached value
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+  
+      inv <- x$getInversion()
+      
+      if (!is.null(inv)) {
+          message("getting cached inversion")
+          return(inv)
+      }
+      
+      inv <- solve(x$get())
+      x$setInversion( inv )
+      inv
+      
 }
